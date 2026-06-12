c2fb46d8d1de5c10230d413f362d4acd3f398c50
Anis Ur Rahman <aniskhan25@gmail.com>
2026-06-12 14:42:19 +0300

    Bind host /usr/lib64 to neutral /opt/hostlibs to avoid overwriting Ubuntu container lib symlink

--- jobs/run_deep_trace_8gcd.sh
+++ jobs/run_deep_trace_8gcd.sh
@@ -24,8 +24,8 @@ export TORCH_HOME="/scratch/${SLURM_JOB_ACCOUNT}/${USER}/torch_home"
 mkdir -p "$TORCH_HOME"
 
 export LUMI_CONTAINER_IMAGE="${LUMI_CONTAINER_IMAGE:-/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260513_121430/lumi-multitorch-full-u24r70f21m50t210-20260513_121430.sif}"
-export LUMI_CONTAINER_BIND_EXTRA="/usr/lib64:/usr/lib64"
-export SINGULARITYENV_LD_LIBRARY_PATH="/usr/lib64"
+export SINGULARITY_BIND="${SINGULARITY_BIND:+${SINGULARITY_BIND},}/usr/lib64:/opt/hostlibs"
+export SINGULARITYENV_LD_LIBRARY_PATH="/opt/hostlibs"
 
 PROFILER_DIR="${PROFILER_DIR:-/scratch/project_462000131/anisrahm/lumi-job-profiler}"
 
